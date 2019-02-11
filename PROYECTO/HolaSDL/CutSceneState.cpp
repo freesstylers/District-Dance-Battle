9b8b8960e5c99ab1c8385b216cf778a3ff675657
IgnOry <iory@ucm.es>
2019-02-11 18:12:24 +0100

    Incluidos estados y máquina correspondiente
    Planteado más como base, muchos metodos comentados y cosas por añadir (faltan cosas como los botones)

---
+++ PROYECTO/HolaSDL/CutSceneState.cpp
@@ -0,0 +1,21 @@
+#include "CutSceneState.h"
+
+
+
+CutSceneState::CutSceneState(): MapState()
+{
+}
+
+
+CutSceneState::~CutSceneState()
+{
+}
+
+void CutSceneState::update()
+{
+}
+
+bool CutSceneState::handleEvent(SDL_Event e)
+{
+	return false;
+}
---
+++ PROYECTO/HolaSDL/MapState.cpp
@@ -0,0 +1,21 @@
+#include "MapState.h"
+
+
+
+MapState::MapState() : GameState()
+{
+}
+
+
+MapState::~MapState()
+{
+}
+
+void MapState::update()
+{
+}
+
+bool MapState::handleEvent(SDL_Event e)
+{
+	return false;
+}
