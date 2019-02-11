c454f54b9110fbef126cdc6e4b5bbdfde5879bd3
javiarias <javari02@ucm.es>
2019-02-11 18:16:09 +0100

    Comentadas algunas clases con métodos o funciones que se podrían añadir

--- PROYECTO/HolaSDL/Pulsador.cpp
+++ PROYECTO/HolaSDL/Pulsador.cpp
@@ -11,9 +11,16 @@ Pulsador::~Pulsador()
 {
 }
 
-void Pulsador::handleInput(Uint32 time, const SDL_Event& event) {
+void Pulsador::handleInput(Uint32 time, const SDL_Event& event) {	// ¿handleinput en flechas?
 
 
+}
+
+int Pulsador::checkAccuracy(double xPosition) {
+	//comprueba la precisión de la nota en la posición incluida como parámetro, devuelve un valor int que determina la precisión
+	//0 bad, 1 good, 2 perfect? Y playstate calcula la puntuación a partir de este valor según la canción
+
+	//esta función es llamada por la flecha desde su handleInput
 }
 
 void Pulsador::update(Uint32 time) {
