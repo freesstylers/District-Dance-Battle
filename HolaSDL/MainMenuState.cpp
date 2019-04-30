1e59cedd77ba4b6142a2cc975cfba84639283a78
oscarpb99 <oscarped@ucm.es>
2019-05-01 00:45:49 +0200

    Funcion del primer boton del menu principal

--- HolaSDL/MainMenuState.cpp
+++ HolaSDL/MainMenuState.cpp
@@ -4,11 +4,12 @@
 
 MainMenuState::MainMenuState(GameManager*g):GameState(g)
 {
+	gameManager = g;
 	controller = SDL_GameControllerOpen(0);
 	EmptyObject* icon = new EmptyObject(g, Vector2D(gameManager->getDefaultWindowWidth() / 2-100, gameManager->getDefaultWindowHeight() / 2 -100), 200, 200, Resources::MenuIcon);
-	buttons[0].first = new EmptyObject(g, Vector2D(gameManager->getDefaultWindowWidth() / 2 , gameManager->getDefaultWindowHeight() / 4*2), 160, 50, Resources::ButtonRestart);
-	buttons[1].first = new EmptyObject(g, Vector2D(gameManager->getDefaultWindowWidth() / 2 , gameManager->getDefaultWindowHeight() / 4*2.5), 160, 50, Resources::ButtonRestart);
-	buttons[2].first = new EmptyObject(g, Vector2D(gameManager->getDefaultWindowWidth() / 2 , gameManager->getDefaultWindowHeight() /4*3), 160, 50, Resources::ButtonRestart);
+	buttons[0].first = new EmptyObject(g, Vector2D(gameManager->getDefaultWindowWidth() / 2 , gameManager->getDefaultWindowHeight() /2+50), 160, 50, Resources::ButtonRestart);
+	buttons[1].first = new EmptyObject(g, Vector2D(gameManager->getDefaultWindowWidth() / 2 , gameManager->getDefaultWindowHeight() / 2+150), 160, 50, Resources::ButtonRestart);
+	buttons[2].first = new EmptyObject(g, Vector2D(gameManager->getDefaultWindowWidth() / 2 , gameManager->getDefaultWindowHeight() /2+250), 160, 50, Resources::ButtonRestart);
 
 	buttons[0].second = true;
 	buttons[1].second = false;
@@ -70,12 +71,12 @@ void MainMenuState::newGame(GameManager* gameManager)
 bool MainMenuState::handleEvent(Uint32 time, SDL_Event e) 
 {
 	bool input = false;
-	if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_DOWN) || e.key.keysym.sym == SDLK_DOWN)
+	if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_UP) || e.key.keysym.sym == SDLK_DOWN)
 	{
 		nextButton();
 		input = true;
 	}
-	else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_UP) || e.key.keysym.sym == SDLK_UP)
+	else if (SDL_GameControllerGetButton(controller, SDL_CONTROLLER_BUTTON_DPAD_DOWN) || e.key.keysym.sym == SDLK_UP)
 	{
 		backButton();
 		input = true;
