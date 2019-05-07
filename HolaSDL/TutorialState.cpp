3b94f50a1e4dc00b3b73abf02dacddd51ef130da
javiarias <javari02@ucm.es>
2019-05-07 17:40:54 +0200

    Wow

---
+++ HolaSDL/TutorialState.cpp
@@ -0,0 +1,13 @@
+#include "TutorialState.h"
+
+
+
+TutorialState::TutorialState(GameManager* g) : PlayState(g, 9, true, false)
+{
+
+}
+
+
+TutorialState::~TutorialState()
+{
+}
---
+++ HolaSDL/TutorialState.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "PlayState.h"
+class TutorialState :
+	public PlayState
+{
+public:
+	TutorialState(GameManager* g);
+	~TutorialState();
+
+
+
+};
+
