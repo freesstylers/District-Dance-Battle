8264c91f8850e99f80ff48fc364597a300693537
javi <javari02@ucm.es>
2019-05-12 16:37:20 +0200

    crisis averted

--- HolaSDL/main.cpp
+++ HolaSDL/main.cpp
@@ -6,9 +6,6 @@ using namespace std;
 
 int main(int ac, char** av) {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	_CrtSetBreakAlloc(21749);
-	_CrtSetBreakAlloc(14277);
-	_CrtSetBreakAlloc(21748);
 
 	setlocale(LC_ALL, "");
 
