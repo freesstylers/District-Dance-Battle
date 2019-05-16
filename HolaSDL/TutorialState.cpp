eeac13c00d0d74b52040dd362df0f11d9d48ed0f
alvarded <alvarded@ucm.es>
2019-05-17 01:23:49 +0200

    Highlights tuto acabados

--- HolaSDL/TutorialState.cpp
+++ HolaSDL/TutorialState.cpp
@@ -12,24 +12,45 @@ TutorialState::TutorialState(GameManager* g) : PlayState(g, 0, true, false)
 	tutoFiltro->setActive(false);
 
 	//first stop
-	note1 = new EmptyObject(g, Vector2D(g->getDefaultWindowWidth() / 2 + 145, g->getDefaultWindowHeight() / 2 - 80), 70, 70, Resources::YButton);
-	note2 = new EmptyObject(g, Vector2D(g->getDefaultWindowWidth() / 2 + 145, 400), 70, 70, Resources::YButton);
-	note3 = new EmptyObject(g, Vector2D(500, 200), 70, 70, Resources::YButton);
+	note1 = new EmptyObject(g, Vector2D(g->getDefaultWindowWidth() / 2 + 57, g->getDefaultWindowHeight() / 2 - 32), 70, 70, Resources::YButton);
+	note2 = new EmptyObject(g, Vector2D(g->getDefaultWindowWidth() / 2 + 57, g->getDefaultWindowHeight() / 2 - 290), 70, 70, Resources::XButton);
+	note3 = new EmptyObject(g, Vector2D(g->getDefaultWindowWidth() / 2 - 123, 206), 70, 70, Resources::YButton);
+	circle1 = new EmptyObject(g, Vector2D(g->getDefaultWindowWidth() / 2 - 119 - 55 / 2, 565), 100, 100, Resources::Point);
+	circle2 = new EmptyObject(g, Vector2D(g->getDefaultWindowWidth() / 2 + 57 - 45 / 2, 565), 100, 100, Resources::Point);
 
 	note1->setActive(false);
 	note2->setActive(false);
 	note3->setActive(false);
+	circle1->setActive(false);
+	circle2->setActive(false);
+
 	//second stop
+	barraPuntos = new EmptyObject(g, Vector2D(6, 600 - 402), 82, 402, Resources::StarBar);
+
+	barraPuntos->setActive(false);
 
 	//third stop
+	barraAmarilla = new EmptyObject(g, Vector2D(50, 35), 600, 14, Resources::YellowBar);
+	nota = new EmptyObject(g, Vector2D(600 + 50 - 13, 32), 18, 22, Resources::NoteIcon);
+
+	barraAmarilla->setActive(false);
+	nota->setActive(false);
 
 	//fourth stop
+	bomba= new EmptyObject(g, Vector2D(g->getDefaultWindowWidth() / 2 + 57, g->getDefaultWindowHeight() / 2 - 205), 70, 70, Resources::FlechaEspBomba);
+	bomba->setActive(false);
 
 	stage.push_back(tutoFiltro);
 	stage.push_back(tutorialBox);
 	stage.push_back(note1);
 	stage.push_back(note2);
 	stage.push_back(note3);
+	stage.push_back(circle1);
+	stage.push_back(circle2);
+	stage.push_back(barraPuntos);
+	stage.push_back(barraAmarilla);
+	stage.push_back(nota);
+	stage.push_back(bomba);
 
 	controller = SDL_GameControllerOpen(0);
 }
@@ -139,6 +160,27 @@ bool TutorialState::pauseTutorial()
 		tutoFiltro->setActive(true);
 		tutorialBox->setActive(true);
 
+		switch (numStops)
+		{
+		case 0:
+			note1->setActive(true);
+			note2->setActive(true);
+			note3->setActive(true);
+			circle1->setActive(true);
+			circle2->setActive(true);
+			break;
+		case 1:
+			barraPuntos->setActive(true);
+			break;
+		case 2:
+			barraAmarilla->setActive(true);
+			nota->setActive(true);
+			break;
+		case 3:
+			bomba->setActive(true);
+			break;
+		}
+
 		auxTime = SDL_GetTicks();
 	}
 
@@ -156,23 +198,36 @@ void TutorialState::resumeTutorial(GameManager* g, unsigned int timePaused)
 		numStops++;
 
 		switch (numStops) {
-		case 1:
+		case 0:
 			tutorialBoxAnim = Resources::TutoBox1;
-			note1->setActive(true);
-			note2->setActive(true);
-			note3->setActive(true);
+			
 			break;
-		case 2:
+		case 1:
 			tutorialBoxAnim = Resources::TutoBox2;
 			tutorialBox->setPosition(Vector2D(120, g->getDefaultWindowHeight() / 2 - 30));
+			note1->setActive(false);
+			note2->setActive(false);
+			note3->setActive(false);
+			circle1->setActive(false);
+			circle2->setActive(false);
+
+			
 			break;
-		case 3:
+		case 2:
 			tutorialBoxAnim = Resources::TutoBox3;
 			tutorialBox->setPosition(Vector2D(g->getDefaultWindowWidth() / 2 - (612 * 0.75)/2, 100));
+
+			barraPuntos->setActive(false);
 			break;
-		case 4:
+		case 3:
 			tutorialBoxAnim = Resources::TutoBox4;
 			tutorialBox->setPosition(Vector2D(g->getDefaultWindowWidth() / 2 + 145, g->getDefaultWindowHeight() / 2 - 80));
+
+			barraAmarilla->setActive(false);
+			nota->setActive(false);
+			break;
+		case 4:
+			bomba->setActive(false);
 			break;
 		}
 
