6689f97e1efe392fe53d2bf483802aea6115baa3
GCidon <38065027+GCidon@users.noreply.github.com>
2019-09-22 15:00:46 +0200

    ExtraMenu.h y .cpp añadido

---
+++ HolaSDL/ExtraMenu.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "GameObject.h"
+#include "EmptyObject.h"
+#include "GameState.h"
+#include "Timer.h"
+#include "Background.h"
+#include <list>
+#include "checkML.h"
+
+//State used to display a menu for selecting the extra levels made for Guerrilla
+
+class PlayState;
+
+class ExtraMenu: public GameState
+{
+private:
+	Timer* timer;
+	SDL_GameController* controller = NULL;
+	double distanceHand;
+	PlayState* level;
+
+	EmptyObject* bg;
+	EmptyObject* hand;
+	EmptyObject* switchPlayers;
+	EmptyObject* select;
+
+	bool onePlayer = true;
+	int posHand;
+
+	bool optionsOpen = false;
+
+	void selectionUp();
+	void selectionDown();
+	void resumeSong() {};
+	void toggleOptions() {};
+	void restartSong() {};
+	void init();
+	void exitSong() {};
+
+public:
+	ExtraMenu(GameManager* game);
+	~ExtraMenu();
+
+	virtual bool handleEvent(Uint32 time,  SDL_Event event);
+	virtual void update(Uint32 time) {};
+	virtual void render(Uint32 time, bool beatSync = false);
+};
+
