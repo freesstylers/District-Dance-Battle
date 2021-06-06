002e1952ae3d90d5cf64d68d72279d0bb95a826e
Álvar de Diego López <alvarded@ucm.es>
2021-06-06 17:04:03 +0200

    pierde extras

--- Python/numVecesPierdeCadaNivel.py
+++ Python/numVecesPierdeCadaNivel.py
@@ -2,7 +2,7 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open('c:/Users/alvar/Documents/GitHub/District-Dance-Battle/Python/infoTest.json') as f:
+with open('c:/Users/alvar/Documents/GitHub/District-Dance-Battle/Python/infoSexy.json') as f:
     data = json.load(f)
 
 lost_level_events = []
@@ -14,7 +14,7 @@ for i in range(len(data["events"])):
             lost_level_events.append(data["events"][i])
             eventAux = data["events"][i]['score']
 
-sumas = [0,0,0,0,0]
+sumas = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 for i in range(len(lost_level_events)):
     if lost_level_events[i]['level'] == 2:
@@ -27,11 +27,42 @@ for i in range(len(lost_level_events)):
         sumas[3] += 1
     if lost_level_events[i]['level'] == 22:
         sumas[4] += 1
+    if lost_level_events[i]['level'] == 21:
+        sumas[5] += 1
+    if lost_level_events[i]['level'] == 12:
+        sumas[6] += 1
+    if lost_level_events[i]['level'] == 28:
+        sumas[7] += 1
+    if lost_level_events[i]['level'] == 24:
+        sumas[8] += 1
+    if lost_level_events[i]['level'] == 11:
+        sumas[9] += 1
+    if lost_level_events[i]['level'] == 34:
+        sumas[10] += 1
+    if lost_level_events[i]['level'] == 27:
+        sumas[11] += 1
+    if lost_level_events[i]['level'] == 30:
+        sumas[12] += 1
+    if lost_level_events[i]['level'] == 15:
+        sumas[13] += 1
+    if lost_level_events[i]['level'] == 26:
+        sumas[14] += 1
+    if lost_level_events[i]['level'] == 29:
+        sumas[15] += 1
+    if lost_level_events[i]['level'] == 10:
+        sumas[16] += 1
+    if lost_level_events[i]['level'] == 25:
+        sumas[17] += 1
+    if lost_level_events[i]['level'] == 9:
+        sumas[18] += 1
+    if lost_level_events[i]['level'] == 23:
+        sumas[19] += 1
 
 
 fig = plt.figure()
 #ax = fig.add_axes([0,0,1,1])
-lvls = [1,2,3,4,5]
+#lvls = [1,2,3,4,5]
+lvls = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
 #y_pos = np.arange(len(lvls))
 #plt.xticks(y_pos, lvls)
 plt.xlabel('Nivel')
